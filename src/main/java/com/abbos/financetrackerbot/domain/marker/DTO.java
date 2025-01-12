652c4f2064a60e9566afdd6f70b0441d747d1a6e
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2025-01-12 16:49:00 +0500

    markers

---
+++ src/main/java/com/abbos/financetrackerbot/domain/marker/DTO.java
@@ -0,0 +1,8 @@
+package com.abbos.financetrackerbot.domain.marker;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 12/January/2025  12:42
+ **/
+public interface DTO {
+}
---
+++ src/main/java/com/abbos/financetrackerbot/domain/marker/Response.java
@@ -0,0 +1,8 @@
+package com.abbos.financetrackerbot.domain.marker;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 12/January/2025  12:43
+ **/
+public interface Response extends DTO {
+}
